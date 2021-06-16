e5a053675b301d3cd480141a50526f13c9aa109e
rodrigoarenas <31422766+rodrigo-arenas@users.noreply.github.com>
2021-06-16 14:35:45 -0500

    anomaly detection model

---
+++ model/train.py
@@ -0,0 +1,17 @@
+from joblib import dump
+
+import numpy as np
+from sklearn.ensemble import IsolationForest
+
+rng = np.random.RandomState(42)
+
+# Generate train data
+X = 0.3 * rng.randn(500, 2)
+X_train = np.r_[X + 2, X - 2]
+
+# fit the model
+clf = IsolationForest(max_samples=500, random_state=rng)
+clf.fit(X_train)
+
+dump(clf, './isolation_forest.joblib')
+
